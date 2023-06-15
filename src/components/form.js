88d17aad1b14666d3bad7bdda59dd6eea1ba69a9
treva556 <marktreva92@gmail.com>
2023-06-15 03:06:51 +0300

    timeout

--- src/components/form.js
+++ src/components/form.js
@@ -1,56 +1,75 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AddUserForm = () => {
-  const [name, setName] = useState('');
-  const [idNumber, setIdNumber] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({
+    name: '',
+    id_number: '',
+    phone_number: '',
+    password: ''
+  });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
 
-    const userData = {
-      name,
-      id_number: idNumber,
-      phone_number: phoneNumber,
-      password,
-    };
-
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/users', {
+      const response = await fetch('http://127.0.0.1:3000/users', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify(userData),
+        body: JSON.stringify(user)
       });
-
-      if (response.ok) {
-        // User created successfully
-        // Perform any necessary actions, such as refreshing the user list
-      } else {
-        // Handle error response
-      }
+      const data = await response.json();
+      console.log(data); // Log the response for debugging
+      // Clear the form after successful submission
+      setUser({ name: '', id_number: '', phone_number: '', password: '' });
     } catch (error) {
-      // Handle network error
+      console.error('Error creating user:', error);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-
-      <label htmlFor="idNumber">ID Number:</label>
-      <input type="text" id="idNumber" value={idNumber} onChange={(e) => setIdNumber(e.target.value)} />
-
-      <label htmlFor="phoneNumber">Phone Number:</label>
-      <input type="text" id="phoneNumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-
-      <button type="submit">Add User</button>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={user.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>ID Number:</label>
+        <input
+          type="text"
+          name="id_number"
+          value={user.id_number}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Phone Number:</label>
+        <input
+          type="text"
+          name="phone_number"
+          value={user.phone_number}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
     </form>
   );
 };
